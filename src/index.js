2f7b0e927319e301babedaea867088d612783e63
kwaknayoung <rhkrskdud1@handong.ac.kr>
2024-11-16 22:47:54 +0900

    ,

--- src/index.js
+++ src/index.js
@@ -5,3 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootElement = document.getElementById('root');    
 ReactDOM.render(<App />, rootElement);
+document.getElementById('customLink').addEventListener('click', (e) => {
+    e.preventDefault(); // 기본 동작 방지
+    console.log('React 앱 내부에서 링크를 처리합니다!');
+});
